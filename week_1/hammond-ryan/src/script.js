360a917c8ed427cc0ddac1f4bb2fe2d9507c8dc1
ryanwhammond <ryan@gorocketcloud.com>
2018-01-25 20:31:54 -0500

    HTML Updates
    Add a little more JS, CSS and HTML

--- week_1/hammond-ryan/src/script.js
+++ week_1/hammond-ryan/src/script.js
@@ -2,9 +2,12 @@
 $(document).ready(function(){
 
 	var image = $("#ryan");
-	$( "#ryan" ).click(function() {
+	var replace = $("#noRyan");
+	$("#ryan").click(function() {
 		image.hide();
 		//out of ideas, its been a long day 
+		alert("Bye bye photo.");
+		replace.show();
 	});
 
 });
