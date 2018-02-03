006b60b6163899bdcc18cd1802e0f7654a337f96
egg899 <germanverissimo5@hotmail.com>
2018-02-02 20:11:21 -0500

    Circles created

--- week_2/verissimo-alberto/src/script.js
+++ week_2/verissimo-alberto/src/script.js
@@ -54,9 +54,15 @@ myMap.addListener('center_changed', function(){
 
 
 for(var i=0;i<locations.length;i++){
-  var marker = new google.maps.Marker({
-    position:locations[i],
+  var marker = new google.maps.Circle({
+    strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+    center:locations[i],
     map:myMap,
+    radius:30,
   })//marker
 
 
