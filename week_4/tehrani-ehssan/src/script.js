aab7b5aefdef7984e583f3896121a78a584d4034
EhssanTehrani <ehssantehrani@gmail.com>
2018-02-22 11:50:47 -0500

    Week 3 Update

---
+++ week_4/tehrani-ehssan/src/script.js
@@ -0,0 +1,32 @@
+$.ajax({
+	type: "GET", //request type
+	dataType: "jsonp", //JSONP is a method for sending JSON data without worrying about cross-domain issues. JSONP does not use the XMLHttpRequest object. 
+	cache: false,
+	url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&near=Toronto&query=tacos',
+	success: function(response){
+		
+		response.response.venues.forEach(function(venue){
+			
+			console.log(response);
+			var location = venue.location;
+			var html = '<p>';
+			html += venue.name;
+			html +='<br>';
+			html +='<img src="';
+			html +='https://maps.googleapis.com/maps/api/staticmap';
+			html +='?zoom=15';
+			html += '&size=600x300';
+			html += '&maptype=roadmap';
+			html += '&center=' + location.lat + ',' + location.lng;
+			//encodeURIComponent is used to encode the || in the URI 
+			html += '&markers=' + encodeURIComponent('||') + location.lat + ',' + location.lng;
+			html +='">';
+			html += '</p>';
+			document.getElementById('output').innerHTML += html;
+
+
+		})
+
+	},
+
+});
